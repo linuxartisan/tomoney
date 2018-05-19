542d6c52e85c69a514de668d2773e7d4f782420f
Sandesh Patil <sandesh87@gmail.com>
2018-05-19 13:47:04 +0530

    Add conversion functionality to Indian and US format
    Conversion functionality for Indian and US currency format is added.
    Changed autoload information in composer.json.

--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,19 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-use Convert\ConvertMoney;
+use \Convert\ConvertMoney;
 
-echo ConvertMoney::toMoney(123456127.738);
+print ConvertMoney::toIndian(123456127.73)."\n";
+print ConvertMoney::toIndian(123456127.731)."\n";
+print ConvertMoney::toIndian(123456127.738)."\n";
+print ConvertMoney::toIndian(-123456.738)."\n";
+print ConvertMoney::toIndian(123.12)."\n";
+print ConvertMoney::toIndian(1234.12)."\n";
+
+print ConvertMoney::toUS(123456127.73)."\n";
+print ConvertMoney::toUS(123456127.731)."\n";
+print ConvertMoney::toUS(123456127.738)."\n";
+print ConvertMoney::toUS(-123456.738)."\n";
+print ConvertMoney::toUS(123.12)."\n";
+print ConvertMoney::toUS(1234.12)."\n";
+
